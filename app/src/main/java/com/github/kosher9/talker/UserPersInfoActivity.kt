9b74058af0f48e14f18ae75395eee68db4d195da
Kosher <lionel.kossi@gmail.com>
2019-10-15 09:18:49 +0000

    Initial commit

---
+++ app/src/main/java/com/github/kosher9/talker/UserPersInfoActivity.kt
@@ -0,0 +1,12 @@
+package com.github.kosher9.talker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserPersInfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_pers_info)
+    }
+}
